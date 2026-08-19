3a9ab8e23b8f9fcb6babff37f1c349f84ac43c6b
Brian Jepson <brian.jepson@raspberrypi.com>
2026-08-19 12:17:18 -0400

    final state, whitespace

--- ch02/math_float.c
+++ ch02/math_float.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 void main (void)
 {
   float a;
   float b = 3.641;
-  int c;
+  float c;
+
   a = 2.897;
   c = a + b;
-  printf ("The sum of adding %f and %f is %d\n", a, b, c);
+  printf ("The sum of adding %f and %f is %f\n", a, b, c);
 }
